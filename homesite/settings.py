18a0939f257cee8aae31586ceee4411506e338a9
DJA753gtb <derinjohnantony@gmail.com>
2023-06-20 12:51:05 +0530

    third commit 3

--- homesite/settings.py
+++ homesite/settings.py
@@ -86,12 +86,12 @@ WSGI_APPLICATION = 'homesite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
@@ -211,3 +211,8 @@ SOCIALACCOUNT_PROVIDERS = {
 
 
 LOGIN_REDIRECT_URL = 'dashboard'
+
+try:
+    from homesite.local_settings import *
+except ImportError:
+    pass    
